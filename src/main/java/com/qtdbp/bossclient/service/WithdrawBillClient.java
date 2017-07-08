7024ebc03fe664e5f5c0a1f5847540c189981cda
caid <caidchen@126.com>
2017-07-08 10:21:19 +0800

    修改提现参数

--- src/main/java/com/qtdbp/bossclient/service/WithdrawBillClient.java
+++ src/main/java/com/qtdbp/bossclient/service/WithdrawBillClient.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -32,6 +31,6 @@ public interface WithdrawBillClient {
      * @return
      */
     @RequestMapping(value = "/api/withdraw", method = RequestMethod.GET)
-    Message findWithdrawBillsByCondtion(@RequestParam HttpServletRequest request) ;
+    Message findWithdrawBillsByCondtion(HttpServletRequest request) ;
 
 }
