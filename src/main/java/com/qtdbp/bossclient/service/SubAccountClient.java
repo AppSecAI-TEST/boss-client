a35cd735615997c5b77c89043f13726f6ca735d6
liyang <18758009334@163.com>
2017-07-11 10:51:40 +0800

    新增查询账户信息

--- src/main/java/com/qtdbp/bossclient/service/SubAccountClient.java
+++ src/main/java/com/qtdbp/bossclient/service/SubAccountClient.java
@@ -1,7 +1,9 @@
 package com.qtdbp.bossclient.service;
 
 import com.qtdbp.bossclient.base.Message;
+import com.qtdbp.bossclient.model.SubAccountSeqModel;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,4 +22,13 @@ public interface SubAccountClient {
      */
     @RequestMapping(value = "/subAccount/info", method = RequestMethod.GET)
     Message findSubAccountInfo(@RequestParam("ssoUserId") String ssoUserId);
+
+    /**
+     * 分页查询用户资金变动流水信息
+     *
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/subAccount/details", method = RequestMethod.POST)
+    Message findAccountSeq(@RequestBody SubAccountSeqModel model);
 }
