4680e314520a0e2db5540146c40c10a4d77e6a46
liyang <18758009334@163.com>
2017-08-09 20:45:16 +0800

    新增撤销交易流水接口

--- src/main/java/com/qtdbp/bossclient/service/BalanceBillClient.java
+++ src/main/java/com/qtdbp/bossclient/service/BalanceBillClient.java
@@ -2,11 +2,13 @@ package com.qtdbp.bossclient.service;
 
 import com.qtdbp.bossclient.base.Message;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 客户对账单
@@ -37,13 +39,13 @@ public interface BalanceBillClient {
     @RequestMapping(value = "/api/balanceBill/audit/async", method = RequestMethod.POST)
     void auditBalanceBillAsync(@RequestParam("ssoIds") List<String> ssoUserIds, @RequestParam("state") String balanceState);
 
+
     /**
      * 异步核算指定日期的服务商对账单接口
-     * @param ssoUserIds 服务商ID列表
-     * @param checkDate 结算日期（T+1），格式：2017-05-25
-     * @param balanceState 结算对账状态，默认null所有，00未对账，10一次结算，11二次结算
+     *
+     * @param map
      * @return
      */
     @RequestMapping(value = "/api/balanceBill/audit/date/async", method = RequestMethod.POST)
-    void auditBalanceBillAsync(@RequestParam("ssoIds") List<String> ssoUserIds, @RequestParam("checkDate") String checkDate, @RequestParam("state") String balanceState);
+    Message auditBalanceBillAsync(@RequestBody Map<String, Object> map);
 }
