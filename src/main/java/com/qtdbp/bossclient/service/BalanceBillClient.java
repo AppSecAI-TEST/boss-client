3659e2000da8ec3d8c1b40ed8ea1f3a9ed19a24f
caid <caidchen@126.com>
2017-08-09 21:06:20 +0800

    修改核算接口

--- src/main/java/com/qtdbp/bossclient/service/BalanceBillClient.java
+++ src/main/java/com/qtdbp/bossclient/service/BalanceBillClient.java
@@ -5,9 +5,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -19,26 +17,24 @@ public interface BalanceBillClient {
 
     /**
      * 同步核算指定日期的服务商对账单接口
-     * @param ssoUserIds 服务商ID列表
-     * @param checkDate 结算日期（T+1），格式：2017-05-25
-     * @param balanceState 结算对账状态，默认null所有，00未对账，10一次结算，11二次结算
+     * @param map
+     *          ssoUserIds 服务商ID列表
+     *          checkDate 结算日期（T+1），格式：2017-05-25
+     *          balanceState 结算对账状态，默认null所有，00未对账，10一次结算，11二次结算
      * @return
      */
     @RequestMapping(value = "/api/balanceBill/audit", method = RequestMethod.POST)
-    Message auditBalanceBill(@RequestParam("ssoIds") List<String> ssoUserIds,
-                             @RequestParam("date") String checkDate,
-                             @RequestParam("state") String balanceState) ;
+    Message auditBalanceBill(@RequestBody Map<String, Object> map) ;
 
     /**
-     * 异步核算指定日期的服务商对账单接口(定时任务调用)
-     * @param ssoUserIds 服务商ID列表
-     * #@param checkDate 结算日期（T+1），格式：2017-05-25
-     * @param balanceState 结算对账状态，默认null所有，00未对账，10一次结算，11二次结算
+     * 异步核算当前时间前一日的服务商对账单接口（定时任务调用）
+     * @param map
+     *          ssoUserIds 服务商ID列表
+     *          balanceState 结算对账状态，默认null所有，00未对账，10一次结算，11二次结算
      * @return
      */
     @RequestMapping(value = "/api/balanceBill/audit/async", method = RequestMethod.POST)
-    void auditBalanceBillAsync(@RequestParam("ssoIds") List<String> ssoUserIds, @RequestParam("state") String balanceState);
-
+    void auditBalanceBillAsyncLastDate(@RequestBody Map<String, Object> map) ;
 
     /**
      * 异步核算指定日期的服务商对账单接口
